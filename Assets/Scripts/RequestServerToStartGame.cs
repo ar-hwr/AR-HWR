dd467d0046873c31e8e1469b3c0f77287ff4bca6
Jessica Kranz <jessica98k@gmail.com>
2019-03-24 18:40:18 +0100

    added LobbyManager

---
+++ Assets/Scripts/RequestServerToStartGame.cs
@@ -0,0 +1,75 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class DataToStartGame
+{
+    public int countPlayers = 2;
+    public int countRounds = 1;
+    public string password = string.Empty;
+    public bool hasPassword = false;
+}
+
+public class RequestServerToStartGame : MonoBehaviour
+{
+
+    public Button startGameButton;
+    public Slider countPlayersSlider;
+    public Slider countRoundsSlider;
+    public InputField passwordInputField;
+
+
+    DataToStartGame serverRequest = new DataToStartGame();
+
+    private string serverURL = "https://jsonplaceholder.typicode.com/todos/1";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        startGameButton.onClick.AddListener(RequestServer);
+    }
+
+    void RequestServer()
+    {  
+        serverRequest.countPlayers = (int)countPlayersSlider.value;
+        serverRequest.countRounds = (int)countRoundsSlider.value;
+        serverRequest.password = passwordInputField.text;
+        if (serverRequest.password != null && serverRequest.password != string.Empty)
+        {
+            serverRequest.hasPassword = true;
+        }
+
+        StartCoroutine(GetText(result => { UnityEngine.Debug.Log(result.ToString()); }));
+
+    }
+
+
+    public IEnumerator GetText(Action<Test> result)
+    {
+        string textToSendToServer = JsonConvert.SerializeObject(serverRequest);
+        // user UnityWebRequest and not Core WebRequests when working with Unity
+        UnityWebRequest request = UnityWebRequest.Post(serverURL, textToSendToServer);
+        UnityEngine.Debug.Log(textToSendToServer);
+
+
+        yield return request.SendWebRequest();
+
+        if (request.isNetworkError || request.isHttpError)
+        {
+            UnityEngine.Debug.Log(request.error);
+        }
+        else
+        {
+            if (result != null)
+            {
+                Test test = JsonConvert.DeserializeObject<Test>(request.downloadHandler.text);
+                result(test);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/SetObjectPositionOnMap.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SetObjectPositionOnMap : MonoBehaviour
+{
+    public GameObject gameObject;
+    private Vector3 position;
+    private Vector3 positionOfParent;
+    
+
+    void Start()
+    {
+        // get position for the object from server
+        // server is not able to tell positions at time of writing this 06/03/19
+        // ~Jessica
+        position = new Vector3(0, 0, 0);
+        //Debug.Log("pos is at the begin " + gameObject.transform.position.x + " " + gameObject.transform.position.y + " " + gameObject.transform.position.z);
+        positionOfParent = new Vector3(gameObject.transform.parent.position.x, gameObject.transform.parent.position.y, gameObject.transform.parent.position.z);
+  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameObject.transform.position.x != position.x ||
+            gameObject.transform.position.y != position.y ||
+            gameObject.transform.position.z != position.z)
+        {
+            positionOfParent = new Vector3(gameObject.transform.parent.position.x, gameObject.transform.parent.position.y, gameObject.transform.parent.position.z);
+
+            SetPosition(gameObject, new Vector3(0.01f, 0, 0));
+            //Debug.Log("pos is now " + gameObject.transform.position.x);
+        }
+    }
+
+    private void SetPosition(GameObject objectToMove, Vector3 positionFromServer)
+    {
+        if(objectToMove.transform.position.x < 1)
+        {
+            objectToMove.transform.position += positionFromServer;
+        }
+        else
+        {
+            objectToMove.transform.position = positionOfParent;
+        }
+
+
+
+    }
+}
