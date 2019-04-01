3ce8fb8426871ab3b93815a636db49ab926b72a7
Jessica Kranz <jessica98k@gmail.com>
2019-04-01 18:20:06 +0200

    example sync var but working but currently without commands to server

--- Assets/Scripts/RequestServerToStartGame.cs
+++ Assets/Scripts/RequestServerToStartGame.cs
@@ -1,75 +1,33 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using TMPro;
 using UnityEngine.Networking;
 using UnityEngine.UI;
 
-public class DataToStartGame
+public class RequestServerToStartGame : NetworkBehaviour
 {
-    public int countPlayers = 2;
-    public int countRounds = 1;
-    public string password = string.Empty;
-    public bool hasPassword = false;
-}
-
-public class RequestServerToStartGame : MonoBehaviour
-{
-
+    [SyncVar]
+    private int health = 100;
+    public TextMeshProUGUI Health;
     public Button startGameButton;
-    public Slider countPlayersSlider;
-    public Slider countRoundsSlider;
-    public InputField passwordInputField;
+    //public Slider countPlayersSlider;
+    //public Slider countRoundsSlider;
+    //public InputField passwordInputField;
 
-
-    DataToStartGame serverRequest = new DataToStartGame();
-
-    private string serverURL = "https://jsonplaceholder.typicode.com/todos/1";
 
     // Start is called before the first frame update
     void Start()
     {
-
         startGameButton.onClick.AddListener(RequestServer);
     }
 
-    void RequestServer()
-    {  
-        serverRequest.countPlayers = (int)countPlayersSlider.value;
-        serverRequest.countRounds = (int)countRoundsSlider.value;
-        serverRequest.password = passwordInputField.text;
-        if (serverRequest.password != null && serverRequest.password != string.Empty)
-        {
-            serverRequest.hasPassword = true;
-        }
-
-        StartCoroutine(GetText(result => { UnityEngine.Debug.Log(result.ToString()); }));
-
+    [Command]
+    void CmdHealth()
+    {
+        health -= 2;
     }
 
-
-    public IEnumerator GetText(Action<Test> result)
+    void RequestServer()
     {
-        string textToSendToServer = JsonConvert.SerializeObject(serverRequest);
-        // user UnityWebRequest and not Core WebRequests when working with Unity
-        UnityWebRequest request = UnityWebRequest.Post(serverURL, textToSendToServer);
-        UnityEngine.Debug.Log(textToSendToServer);
-
-
-        yield return request.SendWebRequest();
-
-        if (request.isNetworkError || request.isHttpError)
-        {
-            UnityEngine.Debug.Log(request.error);
-        }
-        else
-        {
-            if (result != null)
-            {
-                Test test = JsonConvert.DeserializeObject<Test>(request.downloadHandler.text);
-                result(test);
-            }
-        }
+        CmdHealth();
+        Health.SetText(health.ToString());
     }
 }
---
+++ Assets/Scripts/TakeVehicle.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class TakeVehicle : NetworkBehaviour
+{
+
+    [SyncVar]
+    public int subwayMoves = 4;
+
+    [SyncVar]
+    public int bikeMoves = 4;
+
+    [SyncVar]
+    public int busMoves = 4;
+
+    public Text subwayInfo;
+    public Button takeSubwayButton;
+
+    public Text busInfo;
+    public Button takeBusButton;
+
+    public Text bikeInfo;
+    public Button takeBikeButton;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        subwayInfo.text = subwayMoves.ToString();
+        busInfo.text = busMoves.ToString();
+        bikeInfo.text = bikeMoves.ToString();
+    }
+
+    public void ClientTakeSubway()
+    {
+        subwayMoves--;
+        subwayInfo.text = subwayMoves.ToString();
+    }
+
+    public void ClientTakeBus()
+    {
+        busMoves--;
+        busInfo.text = busMoves.ToString();
+    }
+
+    public void ClientTakeBike()
+    {
+        bikeMoves--;
+        bikeInfo.text = bikeMoves.ToString();
+    }
+
+}
