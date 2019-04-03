13685709c58a613dc504f1a8fec9bfac50895725
Jessica Kranz <jessica98k@gmail.com>
2019-04-03 21:18:57 +0200

    player name gets synchronized between scenes

---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+using UnityEngine.UI;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    [SyncVar] public string pName = "player";
+
+    public Text NameInfo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NameInfo.text = pName;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
