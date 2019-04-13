c19799828bdea30728907c284c7d3b1374dd3b37
Jessica Kranz <jessica98k@gmail.com>
2019-04-13 13:17:06 +0200

    players on Jungfernheide get spawend based on dictionary

---
+++ Assets/ShowHidePlayers.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowHidePlayers : MonoBehaviour
+{   
+    public GameObject Thief;
+    public GameObject PoliceBlue;
+    public GameObject PoliceGreen;
+    public GameObject PoliceRed;
+    public GameObject PoliceYellow;
+
+    [HideInInspector] public Dictionary<GameObject, bool> PlayerBools = new Dictionary<GameObject, bool>();
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       PlayerBools.Add(Thief, false);
+       PlayerBools.Add(PoliceBlue, false);
+       PlayerBools.Add(PoliceGreen, false);
+       PlayerBools.Add(PoliceRed, true);
+       PlayerBools.Add(PoliceYellow, false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (var player in PlayerBools)
+        {
+            var renderer = player.Key.GetComponent<Renderer>();
+            if (player.Value)
+            {
+                renderer.enabled = true;
+            }
+            else
+            {
+                renderer.enabled = false;
+            }
+        }
+    }
+}
